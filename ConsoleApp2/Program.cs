799ca2fbb2878e024d5edfff203385bb09efcf6f
Rudy Samuel <rudysa@herbalife.com>
2023-01-05 18:39:50 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsValidXML(); ;
+        }
+
+
+        private static void IsValidXML()
+        {
+            string filePath = "";
+
+            try
+            {
+                var abc = Directory.EnumerateFiles(@"D:\Git\",
+                    "*.resx", SearchOption.AllDirectories);
+
+                foreach (string file in abc)
+                {
+                    filePath = file;
+                    var contents = File.ReadAllText(file);
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.LoadXml(contents);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write("error");
+                Console.WriteLine(filePath);
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
